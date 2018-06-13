52cf49d75c3da3c3a682cf96df6583fe61e7c636
Mobile 3 <luis.fuertes@clouddistrict.com>
2018-06-13 13:20:27 +0200

    Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,87 @@
 const webpack = require('webpack')
-
-const config = {
-  entry: {
-    index: ['./src/index.js']
-  },
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const pkg = require('./package.json')
+const path = require('path')
+const libraryName = pkg.name
+module.exports = {
+  entry: path.join(__dirname, './src/index.js'),
   output: {
-    path: __dirname + '/lib',
-    filename: '[name].js',
+    path: path.join(__dirname, './dist'),
+    filename: 'myUnflappableComponent.js',
+    library: libraryName,
     libraryTarget: 'umd',
-    library: 'cloud-widgets'
+    publicPath: '/dist/',
+    umdNamedDefine: true
   },
-  externals: {
-    react: {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: 'react',
-      amd: 'react'
-    },
-    'react-dom': {
-      root: 'ReactDOM',
-      commonjs: 'react-dom',
-      commonjs2: 'react-dom',
-      amd: 'react-dom'
-    }
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'myUnflappableComponent.css'
+    })
+  ],
+  node: {
+    net: 'empty',
+    tls: 'empty',
+    dns: 'empty'
   },
   module: {
-    loaders: [
+    rules: [
       {
-        test: /.jsx?$/,
-        exclude: /node_modules/,
-        loader: 'babel',
-        query: {
-          presets: ['es2015', 'react', 'stage-0']
-        }
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              fallback: 'file-loader',
+              name: '[name][md5:hash].[ext]',
+              outputPath: 'assets/',
+              publicPath: '/assets/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.*css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: ['babel-loader'],
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(pdf|doc|zip)$/,
+        use: ['file-loader']
       }
     ]
   },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({ comments: false }),
-    new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify('production') }
-    })
-  ]
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, './node_modules/react'),
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+      assets: path.resolve(__dirname, 'assets')
+    }
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: 'react',
+      commonjs2: 'react',
+      amd: 'React',
+      root: 'React'
+    },
+    'react-dom': {
+      commonjs: 'react-dom',
+      commonjs2: 'react-dom',
+      amd: 'ReactDOM',
+      root: 'ReactDOM'
+    }
+  }
 }
-
-module.exports = config
