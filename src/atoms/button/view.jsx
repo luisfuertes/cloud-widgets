fc5950dfd90272782e615a863904e7f381ef8b6f
Mobile 3 <luis.fuertes@clouddistrict.com>
2018-06-14 13:47:52 +0200

    Button added

---
+++ src/atoms/button/view.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react'
+import Spinner from 'react-spinkit'
+
+export default class View extends Component {
+    
+    static defaultProps = {
+        label                 : '',
+        onClick               : null,
+        isFetching            : false,
+        enabled               : true,
+    }
+
+    _handleClick(e){
+        if (this.props.enabled && !this.props.isFetching){
+            this.props.onClick && this.props.onClick(e)
+        }
+    }
+
+    _renderContent(){
+        if( this.props.isFetching ) {
+            return (
+                <span className="button-label">
+                    <Spinner 
+                        color         = { '#11A8AD' }
+                        name          = { 'circle' }
+                        fadeIn        = { 'none' }
+                    />
+                </span>
+            )
+        } else {
+            return (
+                <span className="button-label">
+                    { this.props.label }
+                </span>
+            )
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <button 
+                    type      = { this.props.onClick ? 'button' : 'submit' }
+                    onClick   = { this._handleClick.bind(this) } 
+                >
+                    { this._renderContent() }
+                </button>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import TextInput from './atoms/textinput/'
 import TextareaInput from './atoms/textareainput/'
 import SelectInput from './atoms/selectinput/'
 import MultiselectInput from './atoms/multiselectinput/'
+import Button from './atoms/button/'
 
-export { TextInput, TextareaInput, SelectInput, MultiselectInput }
+export { TextInput, TextareaInput, SelectInput, MultiselectInput, Button }
